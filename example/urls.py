1f3482bcfa4faabdafd618f49fa2f482875435a4
Priy <priy@crunchrapps.com>
2016-05-15 19:33:56 +0200

    remove pycache and add comment on csrf exempt

--- example/urls.py
+++ example/urls.py
@@ -4,5 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
+    # csrf_exempt to be able to test the view with postman
     url(r'^$', csrf_exempt(views.ExampleView.as_view())),
 ]
