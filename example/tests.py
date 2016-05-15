24f656fb78c24696dc845e31e284f943807b387c
Priy <priy@crunchrapps.com>
2016-05-15 19:43:08 +0200

    add gitignore, add test and remove compiled files

--- example/tests.py
+++ example/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from example.views import ExampleView
 
-# Create your tests here.
+class ExampleTestCase(TestCase):
+  
+  def test_example_correct(self):
+    request = {}
+    param1 = 1
+    param2 = 2
+    
+    result = ExampleView().process_post(request, param1, param2)
+    
+    self.assertEqual(result['key'], param1+param2)
